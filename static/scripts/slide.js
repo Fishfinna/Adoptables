cbfad6b5251d011fd1b452824dcadff8cefa5d50
Coburn Nguyen <coburnnguyen@gmail.com>
2022-05-25 02:49:21 -0700

    Add js to automatically transition betweeen image

--- static/scripts/slide.js
+++ static/scripts/slide.js
@@ -26,3 +26,18 @@ function SlideShow(n) {
   slides[slidePosition-1].style.display = "block";
   circles[slidePosition-1].className += " enable";
 } 
+
+var slidePosition = 0;
+SlideShow();
+
+function SlideShow() {
+  var i;
+  var slides = document.getElementsByClassName("Image-container");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slidePosition++;
+  if (slidePosition > slides.length) {slidePosition = 1}
+  slides[slidePosition-1].style.display = "block";
+  setTimeout(SlideShow, 4000); 
+} 
